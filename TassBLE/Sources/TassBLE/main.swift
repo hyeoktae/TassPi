dc41a06f86173770c1220ed8f896a826a9c2deea
hyeoktae kwon <kht_0218@hanmail.net>
2021-06-22 00:44:45 +0900

    feat: tass - fail

--- TassBLE/Sources/TassBLE/main.swift
+++ TassBLE/Sources/TassBLE/main.swift
@@ -8,31 +8,6 @@
 import Foundation
 
 @discardableResult
-//func shell(_ args: String...) -> Int32 {
-//    let task = Process()
-//    task.launchPath = "~/usr/bin/env"
-//    task.arguments = args
-//    task.launch()
-//    task.waitUntilExit()
-//    return task.terminationStatus
-//}
-
-//func shell(_ command: String) -> String {
-//    let task = Process()
-//    let pipe = Pipe()
-//
-//    task.standardOutput = pipe
-//    task.standardError = pipe
-//    task.arguments = ["-c", command]
-//    task.launchPath = "/bin/env"
-//    task.launch()
-//
-//    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-//    let output = String(data: data, encoding: .utf8)!
-//
-//    return output
-//}
-
 func shell(command: String) -> Int32 {
   let task = Process()
   task.launchPath = "/usr/bin/env"
@@ -42,7 +17,7 @@ func shell(command: String) -> Int32 {
   return task.terminationStatus
 }
 
-let state = shell(command: "raspistill -o tassImg.jpg")
+let state = shell(command: "raspistill - tassImg.jpg")
 print("state: ", state)
 //if #available(macOS 10.15, *) {
 //  let camera = CameraManager()
