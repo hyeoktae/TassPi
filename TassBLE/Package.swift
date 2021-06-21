4cee9fe10c662b603751de51b2301bf223a537f2
hyeoktae kwon <kht_0218@hanmail.net>
2021-06-22 02:24:28 +0900

    feat: tass - remove

--- TassBLE/Package.swift
+++ TassBLE/Package.swift
@@ -22,6 +22,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TassBLE",
-            dependencies: ["GATT", "SwiftyGPIO", "Bluetooth", "SwiftFoundation"])
+            dependencies: ["GATT", "SwiftyGPIO", "Bluetooth"])
     ]
 )
