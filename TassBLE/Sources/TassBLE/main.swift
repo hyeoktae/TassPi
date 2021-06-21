ec004daa04a08d4a995c41e410f3ceafbdb9e967
hyeoktae kwon <kht_0218@hanmail.net>
2021-06-21 23:55:11 +0900

    feat: tass - bash

--- TassBLE/Sources/TassBLE/main.swift
+++ TassBLE/Sources/TassBLE/main.swift
@@ -17,23 +17,32 @@ import Foundation
 //    return task.terminationStatus
 //}
 
-func shell(_ command: String) -> String {
-    let task = Process()
-    let pipe = Pipe()
-    
-    task.standardOutput = pipe
-    task.standardError = pipe
-    task.arguments = ["-c", command]
-    task.launchPath = "/bin/env"
-    task.launch()
-    
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8)!
-    
-    return output
+//func shell(_ command: String) -> String {
+//    let task = Process()
+//    let pipe = Pipe()
+//
+//    task.standardOutput = pipe
+//    task.standardError = pipe
+//    task.arguments = ["-c", command]
+//    task.launchPath = "/bin/env"
+//    task.launch()
+//
+//    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+//    let output = String(data: data, encoding: .utf8)!
+//
+//    return output
+//}
+
+func shell(command: String) -> Int32 {
+  let task = Process()
+  task.launchPath = "/usr/bin/env"
+  task.arguments = ["bash", "-c", command]
+  task.launch()
+  task.waitUntilExit()
+  return task.terminationStatus
 }
 
-let state = shell("raspistill -o tassImg.jpg")
+let state = shell(command: "raspistill -o tassImg.jpg")
 print("state: ", state)
 //if #available(macOS 10.15, *) {
 //  let camera = CameraManager()
