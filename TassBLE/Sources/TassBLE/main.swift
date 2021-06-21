d7a694b5c4d49ef5e75775fe528afe420e3be2a8
hyeoktae kwon <kht_0218@hanmail.net>
2021-06-21 23:47:44 +0900

    feat: tass - test2

--- TassBLE/Sources/TassBLE/main.swift
+++ TassBLE/Sources/TassBLE/main.swift
@@ -10,15 +10,15 @@ import Foundation
 @discardableResult
 func shell(_ args: String...) -> Int32 {
     let task = Process()
-    task.launchPath = "~/TassPi/TassBLE/Sources/TassBLE"
+    task.launchPath = "~/usr/bin/env"
     task.arguments = args
     task.launch()
     task.waitUntilExit()
     return task.terminationStatus
 }
 
-shell("raspistill -o tassImg.jpg")
-
+let state = shell("raspistill -o tassImg.jpg")
+print("state: ", state)
 //if #available(macOS 10.15, *) {
 //  let camera = CameraManager()
 //  camera.startCapture()
