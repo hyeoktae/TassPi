d2bfd66f00db6272e375cb0440c06546ae9151aa
hyeoktae kwon <kht_0218@hanmail.net>
2021-06-22 23:49:05 +0900

    feat: tass - test2

--- TassBLE/Sources/TassBLE/BleModule.swift
+++ TassBLE/Sources/TassBLE/BleModule.swift
@@ -1,33 +1,33 @@
-////
-////  File.swift
-////
-////
-////  Created by hyeoktae kwon on 2021/06/22.
-////
 //
-//import Foundation
-//import Bluetooth
-//import BluetoothGATT
+//  File.swift
 //
 //
-//let ble = BleModule.shared
+//  Created by hyeoktae kwon on 2021/06/22.
 //
-//class BleModule {
-//  static let shared = BleModule()
-//
-//  var bleUUID: BluetoothUUID? = nil
-//
-//  func setup(uuidStr: String) {
-//    let add = BluetoothAddress(rawValue: "B8:27:EB:06:1F:C2") ?? BluetoothAddress()
-//    let l2Cap = L2CAP(address: add, name: "TassPi")
-//    let server = GATTServer(socket: l2Cap)
-//    try? server.read()
-//    server.writeValue(<#T##value: Data##Data#>, forCharacteristic: <#T##BluetoothUUID#>)
-//
-//    let uuid128bit = BluetoothUUID(rawValue: "0000110e-0000-1000-8000-00805f9b34fb")
-//    let uuid = UUID(uuidString: "0000110e-0000-1000-8000-00805f9b34fb") ?? UUID()
-//    self.bleUUID = BluetoothUUID(uuid: uuid)
-//    let temp = Bluetooth.AppleBeacon.init(uuid: uuid, major: 1, minor: 2, rssi: 3)
-//
-//  }
-//}
+
+import Foundation
+import Bluetooth
+import BluetoothGATT
+
+
+let ble = BleModule.shared
+
+class BleModule {
+  static let shared = BleModule()
+
+  var bleUUID: BluetoothUUID? = nil
+
+  func setup(uuidStr: String) {
+    let add = BluetoothAddress(rawValue: "B8:27:EB:06:1F:C2") ?? BluetoothAddress()
+    let l2Cap = L2CAP(address: add, name: "TassPi")
+    let server = GATTServer(socket: l2Cap)
+    try? server.read()
+    server.writeValue(<#T##value: Data##Data#>, forCharacteristic: <#T##BluetoothUUID#>)
+
+    let uuid128bit = BluetoothUUID(rawValue: "0000110e-0000-1000-8000-00805f9b34fb")
+    let uuid = UUID(uuidString: "0000110e-0000-1000-8000-00805f9b34fb") ?? UUID()
+    self.bleUUID = BluetoothUUID(uuid: uuid)
+    let temp = Bluetooth.AppleBeacon.init(uuid: uuid, major: 1, minor: 2, rssi: 3)
+
+  }
+}
--- TassBLE/Sources/TassBLE/L2Cap.swift
+++ TassBLE/Sources/TassBLE/L2Cap.swift
@@ -1,70 +1,70 @@
-////
-////  File.swift
-////  
-////
-////  Created by hyeoktae kwon on 2021/06/22.
-////
 //
-//import Foundation
-//import Bluetooth
-//import BluetoothGATT
+//  File.swift
 //
-///// Test L2CAP socket
-//final class L2CAP: L2CAPSocketProtocol {
-//    
-//    // MARK: - Properties
-//    
-//    var log: ((String) -> ())?
-//    
-//    let name: String
-//    
-//    let address: BluetoothAddress
-//    
-//    /// The socket's security level.
-//    private(set) var securityLevel: SecurityLevel = .sdp
-//    
-//    /// Attempts to change the socket's security level.
-//    func setSecurityLevel(_ securityLevel: SecurityLevel) throws {
-//        
-//        self.securityLevel = securityLevel
-//    }
-//    
-//    /// Target socket.
-//    weak var target: L2CAP?
-//    
-//    private(set) var input = [Data]()
-//    
-//    private(set) var cache = [Data]()
-//    
-//    init(address: BluetoothAddress = .zero,
-//         name: String = "") {
-//        
-//        self.address = address
-//        self.name = name
-//    }
-//    
-//    // MARK: - Methods
-//    
-//    /// Write to the socket.
-//    func send(_ data: Data) throws {
-//        
-//        guard let target = self.target
-//            else { throw POSIXError(.ECONNRESET) }
-//        
-//        target.input.append(data)
-//    }
-//    
-//    /// Reads from the socket.
-//    @discardableResult
-//    func recieve(_ bufferSize: Int) throws -> Data? {
-//        
-//        guard let sentData = self.input.popFirst()
-//            else { return nil }
-//        
-//        let readData = Data(sentData.prefix(bufferSize))
-//        
-//        cache.append(readData)
-//        
-//        return readData
-//    }
-//}
+//
+//  Created by hyeoktae kwon on 2021/06/22.
+//
+
+import Foundation
+import Bluetooth
+import BluetoothGATT
+
+/// Test L2CAP socket
+final class L2CAP: L2CAPSocketProtocol {
+    
+    // MARK: - Properties
+    
+    var log: ((String) -> ())?
+    
+    let name: String
+    
+    let address: BluetoothAddress
+    
+    /// The socket's security level.
+    private(set) var securityLevel: SecurityLevel = .sdp
+    
+    /// Attempts to change the socket's security level.
+    func setSecurityLevel(_ securityLevel: SecurityLevel) throws {
+        
+        self.securityLevel = securityLevel
+    }
+    
+    /// Target socket.
+    weak var target: L2CAP?
+    
+    private(set) var input = [Data]()
+    
+    private(set) var cache = [Data]()
+    
+    init(address: BluetoothAddress = .zero,
+         name: String = "") {
+        
+        self.address = address
+        self.name = name
+    }
+    
+    // MARK: - Methods
+    
+    /// Write to the socket.
+    func send(_ data: Data) throws {
+        
+        guard let target = self.target
+            else { throw POSIXError(.ECONNRESET) }
+        
+        target.input.append(data)
+    }
+    
+    /// Reads from the socket.
+    @discardableResult
+    func recieve(_ bufferSize: Int) throws -> Data? {
+        
+        guard let sentData = self.input.popFirst()
+            else { return nil }
+        
+        let readData = Data(sentData.prefix(bufferSize))
+        
+        cache.append(readData)
+        
+        return readData
+    }
+}
